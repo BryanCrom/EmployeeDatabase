622c2da12281692fff60d32e1448e889aa7afb6b
Bryan <BryanCrombach@gmail.com>
2024-05-14 21:53:28 +1200

    initial commit

---
+++ employee-management-system/employee-system-api/src/test/java/com/bryan/employeesystemapi/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.employeesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
